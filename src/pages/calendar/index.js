71d6c673f2ed8529bd21eebece45519558f9736d
YIZE <136076063@qq.com>
2019-01-22 10:35:48 +0800

    日历

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './index.vue';
 import '../../util/common';
-
+import '../../assets/css/calendar.css';
 Vue.config.productionTip = false;
 
 new Vue({
--- src/pages/calendar/index.vue
+++ src/pages/calendar/index.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>aaa</div>
+    <section>
+        <div class="calendar-top">
+            <div v-for="(item, index) in week" :key="index">{{item}}</div>
+        </div>
+        <div class="calendar-box">
+            <div v-for="(item, index) in days" :key="index">{{item}}</div>
+        </div>
+    </section>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            week: ['一', '二', '三', '四', '五', '六', '日'],
+            days: []
+        };
+    }
+};
+</script>
